6991af074100168b98ef04be124a8f864b571f91
epicglottis <tom.macgregor@gmail.com>
2015-04-13 22:46:07 -0700

    Update newinstall.sh

--- newinstall.sh
+++ newinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Installing tools and dependencies..."
-sudo apt-get install -qq vim tmux git rxvt-unicode-256color exuberant-ctags
+sudo apt-get install -qq vim tmux git rxvt-unicode-256color exuberant-ctags aptitude curl
 
 echo "Removing local dotfiles..."
 cd
@@ -14,6 +14,23 @@ git init
 git remote add origin https://github.com/epicglottis/dotfiles.git
 git pull origin master
 rm -rf .git
+source ~/.bashrc
+
+echo "Cloning scripts..."
+mkdir -p ~/scripts
+cd ~/scripts
+git init
+git remote add origin https://github.com/epicglottis/scripts.git
+git pull origin master
+rm -rf .git
+
+echo "Grabbing fonts..."
+mkdir -p /tmp/fonts
+cd /tmp/fonts
+wget --quiet "https://github.com/adobe-fonts/source-code-pro/archive/1.017R.zip"
+unzip -qo 1.017R.zip
+sudo cp source-code-pro-1.017R/OTF/*.otf /usr/local/share/fonts/
+fc-cache -f
 
 echo "Installing Vundle..."
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
